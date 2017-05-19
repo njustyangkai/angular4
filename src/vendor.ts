ab16a5ad4ce516fb0f9d6b6aee44aefc564d8ae2
杨恺 <njustyangkai@126.com>
2017-05-19 21:35:16 +0800

    xs

--- src/vendor.ts
+++ src/vendor.ts
@@ -10,5 +10,4 @@ import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
 import '../public/css/index.css';
